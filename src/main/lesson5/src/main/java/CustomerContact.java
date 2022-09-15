0096ef89f573ed50c928cc669e7e8330207886d2
mpasichniuk <marypasichnyuk@gmail.com>
2022-09-15 18:44:32 +0200

    lesson5

---
+++ src/main/lesson5/src/main/java/CustomerContact.java
@@ -0,0 +1,43 @@
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "contacts")
+@NoArgsConstructor
+public class CustomerContact {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(nullable = false)
+        @Enumerated(EnumType.STRING)
+        private ContactType type;
+
+        @Column(nullable = false)
+        private String value;
+
+        @ManyToOne
+        private ProductSpringDTO productSpringDTO;
+
+        public CustomerContact(ContactType type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Contact{" +
+                    "id=" + id +
+                    ", type=" + type +
+                    ", value='" + value + '\'' +
+                    ", userId=" + productSpringDTO.getId() +
+                    '}';
+        }
+    }
+
